b0fe4edb99b7c407cbfdee59ab4d548bba089c95
James Thomson <james.thomson@ramboll.co.uk>
2022-01-24 18:46:41 +0000

    labers and more

--- Inport_Data.py
+++ Inport_Data.py
@@ -2,7 +2,9 @@ import pandas as pd
 from ladybug.epw import EPW
 import sys 
 import PySimpleGUI as sg
-
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, FigureCanvasAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 def Loadfile():
     if len(sys.argv) == 1:
@@ -23,6 +25,53 @@ def Loadfile():
 EP_File=Loadfile()
 
 epw_data = EPW(EP_File)
-dry_bulb_temp = epw_data.dry_bulb_temperature
+dry_bulb_temp = epw_data.dry_bulb_temperature.values
 
-print(dry_bulb_temp)
+
+
+dt = epw_data.dry_bulb_temperature.datetimes
+data={'Date':list(dt),'Dry Build Temperature':list(dry_bulb_temp)}
+
+df = pd.DataFrame(data).set_index('Date')
+df['Dry Build Temperature']=df.round(0).astype(int)
+#value_bins = df['Bins'].value_counts()
+#print(value_bins)
+
+#Count=df.groupby('Bins').Bins.nunique()
+
+#print(Count)
+
+#value_bins.plot.hist(bins=32)
+
+
+fig, ax = plt.subplots()
+plt.figure(figsize=(14,7))
+counts, bins, patches = ax.hist(df,bins=30 , facecolor = '#2ab0ff', edgecolor='#169acf', linewidth=0.5,rwidth=0.9)
+
+# Set the ticks to be at the edges of the bins.
+ax.set_xticks(bins)
+
+for bar in patches:
+    if bar.get_x() < 5:
+        bar.set_facecolor("cornflowerblue")
+    elif bar.get_x()>18:
+        bar.set_facecolor("firebrick")
+    else:
+        bar.set_facecolor("C1")
+
+rects = ax.patches
+labels = ["%dhrs" % i for i in counts]
+
+for rect, label in zip(rects, labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height+0.01, label,
+            ha='center', va='bottom')
+
+plt.style.use('seaborn-whitegrid') # nice and clean grid
+#plt.hist(x,bins=30, facecolor = '#2ab0ff', edgecolor='#169acf', linewidth=0.5,rwidth=0.9)
+#plt.title('Normal Distribution') 
+
+plt.show()
+
+print(df)
+
